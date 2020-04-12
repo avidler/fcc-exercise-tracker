87fcc2dab86c7f7026fe95f42ad4504d91cd5093
avidler <a.vidler@live.co.uk>
2020-04-12 12:58:39 +0100

    Refactor exercise results

--- server.js
+++ server.js
@@ -83,14 +83,20 @@ app.get('/api/exercise/log/:id/:from?/:to?/:limit?', (req, res) => {
   let fromDate = req.query.from ? new Date(req.query.from) : 0
   let toDate = req.query.to ? new Date(req.query.to) : new Date()
   let limit = req.query.limit ? parseInt(req.query.limit) : 0
-
+  
   User.findOne({_id:userId})
   .then((user) => {
+    let username = user.username
     let results = user.exercise
     results = results.filter((item) => item.date > fromDate && item.date < toDate)
     if (limit > 0) {results = results.slice(0, limit)}
     console.log("results.length: ", results.length)
-    res.json({results, count: results.length})
+    res.json({
+      _id: userId, 
+      username,
+      count: results.length,
+      results
+    })
   })
    
   
